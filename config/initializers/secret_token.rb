535e9535374cb93a74cf88bee040de749e3bd01a
jahongir <ajahongir@ajahongir.(none)>
2011-09-14 19:45:32 +0500

    project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garant::Application.config.secret_token = 'bbb80b52bac8c7c69e4ff09af55ac5091f628242220d3b4605fc88598375cfcbe707edd5be499a3fc43ced5e36233f5d4f5effdd7ef30da39fe3584ac4edaf05'
+
