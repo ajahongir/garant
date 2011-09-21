8128ce68f7f88bf67743dba338e49ca05c2dba9a
jahongir <ajahongir@ajahongir.(none)>
2011-09-21 10:13:54 +0500

    changes

--- db/seeds/refinery_sretting.rb
+++
@@ -1,9 +0,0 @@
-refinery_setting = RefinerySetting.find(3)
-refinery_setting.value = [:ru]
-refinery_setting = RefinerySetting.find(10)
-refinery_setting.value = [:ru]
-refinery_setting = RefinerySetting.find(11)
-refinery_setting.value = [:ru]
-refinery_setting = RefinerySetting.find(12)
-refinery_setting.value = [:ru]
-
